29b363bd0e7b3b0a4fffce21492eb910f88030b6
tomcorey26 <tomcorey26@gmail.com>
2019-07-30 14:52:32 -0400

    added some ideas

--- fight/app.js
+++ fight/app.js
@@ -1,1 +1,61 @@
+//IDEAS
+// add feature where user can input where  they want to hunt
+// for wild pokemon
+// give percentage chance for rare types of pokemon
+// can use items like weed lol to increase your chances of catching
+//have option to watch adds to get poke balls
+//computer lab / lazy at work game
+//have the pokemon you catch be rappers
+// play their songs in the battle section
+// have them say their funny lines as their "pokemon cry"
+// have place where all their pokemon are stored
+// have option to play friends
+
+//pokemon battle requirements
+
+//animation for beginning of the battle
+//top and bottom div slide in
+//animation of pokemon being thrown
+
+//as a user i can...
+
+//input a custom name for my player
+
+//choose wether to fight, use and item, or change pokemon
+//modal comes up for pokemon/ items
+
+//choose what attack i can usez
+
+//see the type of the pokemon i have
+
+//see my own and the enemys hp
+
+//use super effective attacks depending on the type
+
+// know wether I have won or lost
+
+//
+
+//UI CLASS
+//initiates animations
+//displays when game is over
+// if you won / lost
+
+// GAME CLASS
+//holds game state
+//checks if game is over or not
+//checks if pokemon has fainted
+
+//POKEMON CLASS
+//properties
+//health
+//condition(like psn/paralzed)
+//type
+//attacks and their damages (object) and their pp
+//method use normal attack
+//method use status inflicting attack
+// heal (item as parameter)
+
+//checks when to play animations
+
 console.log("fart");
