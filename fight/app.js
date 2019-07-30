9557916f94e5e01f6361010f787a5f3736d6b055
tomcorey26 <tomcorey26@gmail.com>
2019-07-30 18:01:48 -0400

    damage multiplier working

--- fight/app.js
+++ fight/app.js
@@ -44,17 +44,50 @@ class Fighter {
     this.attacks = attacks;
     this.condition = "healthy";
   }
-  checkEffectiveness(moveType, enemyType) {}
+  checkEffectiveness(moveType, enemyType) {
+    //TODO add multiplier based on attacktype
+    let multiplier = {
+      superEffective: 1.3,
+      normal: 1,
+      notEffective: 0.7
+    };
+
+    if (moveType == enemyType) {
+      return normal;
+    } else if (moveType == "rock") {
+      if (enemyType == "paper") {
+        return multiplier.notEffective;
+      } else {
+        return multiplier.superEffective;
+      }
+    } else if (moveType == "scissors") {
+      if (enemyType == "rock") {
+        return multiplier.notEffective;
+      } else {
+        return multiplier.superEffective;
+      }
+    } else if (moveType == "paper") {
+      if (enemyType == "scissors") {
+        return multiplier.notEffective;
+      } else {
+        return multiplier.superEffective;
+      }
+    }
+  }
 
   useAttack(attack, enemy) {
     //play attack animation
 
+    //damage multiplier depending on type
+    let damage = attack.damage;
+    let damageMultiplier = this.checkEffectiveness(attack.type, enemy.type);
+    damage *= damageMultiplier;
+
     //calculate hit or miss
-    let onTarget;
     let randomNum = Math.random();
     if (randomNum < attack.accuracy) {
       //decrement enemy health
-      enemy.health -= attack.damage;
+      enemy.health -= damage;
     } else {
       console.log("attack missed!");
     }
--- fight/app.test.js
+++ fight/app.test.js
@@ -4,6 +4,8 @@ test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+//test if each damage multiplier works
+
 //when player has no pokemon left game is over
 
 //when enemy has no pokemon left game is over
