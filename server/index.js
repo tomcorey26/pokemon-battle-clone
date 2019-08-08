eac531c6f755b8060616fc52cc1997301be31ae2
tomcorey26 <tomcorey26@gmail.com>
2019-08-08 19:51:34 -0400

    meep

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const public = path.join(__dirname, "../client");
+const public = path.join(__dirname, "../client/home");
 
 app.use(express.static(public));
 
-app.get("/", function(req, res) {
+app.get("/test", function(req, res) {
   res.send("Hello World");
 });
 
